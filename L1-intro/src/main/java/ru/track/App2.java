b6400a22f933498c2e88064119e11391cd5dea35
Roman <avatar51@mail.ru>
2018-02-24 23:09:58 +0300

    Task1: intro

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,15 +1,23 @@
 package ru.track;
 
-
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Arrays;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 
 public class App2 {
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
+    public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_GITHUB = "github";
+    public static final String FIELD_EMAIL = "email";
+
+    public static void main(String[] args) throws Exception{
+        HttpResponse <JsonNode> r = Unirest.post(URL)
+                .header("accept", "application/json")
+                .field (FIELD_NAME,"Roma")
+                .field (FIELD_GITHUB, "https://github.com/jokerety")
+                .field (FIELD_EMAIL, "maslov.roman.phystech@mail.ru")
+                .asJson();
+        System.out.println( r.getBody().getObject().get("success"));
     }
 }

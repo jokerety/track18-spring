f6f275d3c220ea2c0b82f456f1f9426faa080411
Roman <avatar51@mail.ru>
2018-05-02 14:43:22 +0300

    broadcast 1v

--- L7_threads/src/main/java/ru/track/prefork/Client.java
+++ L7_threads/src/main/java/ru/track/prefork/Client.java
@@ -1,36 +1,86 @@
 package ru.track.prefork;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.io.IOUtils;
+
+
 
 public class Client {
     static Logger log = LoggerFactory.getLogger(Client.class);
-
+    private Socket socket = null;
     private int port;
     private String host;
+
+    Thread writer = new Thread(() -> write());
+    Thread reader = new Thread(() -> read());
 
     public Client(int port, String host) {
         this.port = port;
         this.host = host;
     }
 
-    public void loop() throws Exception {
-        Socket socket = new Socket(host, port);
-        final OutputStream out = socket.getOutputStream();
-        Scanner scanner = new Scanner(System.in);
+    public void write()
+    {
+        try{
 
-        while (true) {
-            String line = scanner.nextLine();
-            if ("q".equals(line)) {
-                break;
+            final InputStream in = socket.getInputStream();
+            byte[] buf = new byte[1024];
+            while (true)
+            {
+                int nRead = in.read(buf);
+                System.out.println(new String (buf, 0 ,nRead));
             }
-            out.write(line.getBytes());
-            out.flush();
         }
+        catch (IOException e)
+        {
+            System.out.println("cannot write");
+        }
+
+    }
+    public void read()
+    {
+        try{
+            final OutputStream out = socket.getOutputStream();
+            Scanner scanner = new Scanner(System.in);
+            while (true)
+            {
+                String line = scanner.nextLine();
+                if ("exit".equals(line)) {
+                    break;
+                }
+                out.write(line.getBytes());
+                out.flush();
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("cannot read ");
+        }
+    }
+
+    public void loop() {
+
+        try{
+            socket = new Socket(host, port);
+            reader.start();
+            writer.start();
+        }
+        catch (IOException e)
+        {
+            throw  new RuntimeException(e);
+        }
+        //finally
+        //{
+        //    IOUtils.closeQuietly(socket);
+       // }
+
     }
 
     public static void main(String[] args) throws Exception {
--- L7_threads/src/main/java/ru/track/prefork/Server.java
+++ L7_threads/src/main/java/ru/track/prefork/Server.java
@@ -1,12 +1,21 @@
 package ru.track.prefork;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
+
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 
 /**
@@ -17,24 +26,137 @@ import org.slf4j.LoggerFactory;
  * - save threads
  * - broadcast (fail-safe)
  */
+class Message {
+    private String data;
+    private long time;
+    private String senderName;
+
+    public Message(String data, long time, String senderName)
+    {
+        this.data = data;
+        this.senderName = senderName;
+        this.time = time;
+    }
+    public String getData()
+    {
+        return data;
+    }
+    public long getTime()
+    {
+        return time;
+    }
+    public String getSenderName()
+    {
+        return senderName;
+    }
+}
+
+class Potok extends Thread {
+    private static int order;
+    public Socket socket;
+    public List<Message> messages;
+    public List<Potok> connections;
+    public Potok(Socket socket, List<Message> messages, List<Potok> connections) {
+        this.socket = socket;
+        this.messages = messages;
+        this.connections = connections;
+    }
+    public void broadcast(Message message) {
+       //for (Message message : messages)
+
+
+                try{
+
+                    for (Potok connection : connections)
+                    {
+                        if (!message.getSenderName().equals(connection.getName()))
+                        {
+                        OutputStream outputStream = connection.socket.getOutputStream();
+                        outputStream.write(message.getData().getBytes());
+                        }
+                    }
+                    }
+
+                catch(IOException e)
+                {
+                    System.out.println("cannot broadcast");
+                }
+
+
+
+    }
+    @Override
+    public void run() {
+
+        try {
+            while (!isInterrupted()) {
+                inc();
+                InputStream inputStream = socket.getInputStream();
+
+                byte[] buf = new byte[1024];
+                if (inputStream.available() != 0) {
+                    sleep(200);
+                }
+                int nRead = inputStream.read(buf);
+                if (nRead == -1) {
+                    System.out.println("cannot read");
+                    break;
+                }
+                System.out.println(new String(buf, 0, nRead));
+                Message message = new Message(new String(buf, 0, nRead), new Date().getTime(), getName());
+               //messages.add(message);
+                broadcast(message);
+            }
+        } catch (InterruptedException e) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            interrupt();
+        }
+
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public synchronized void inc() {
+        order++;
+    }
+}
+
 public class Server {
     static Logger log = LoggerFactory.getLogger(Server.class);
 
+    List<Potok> list = new ArrayList<Potok>();
+    List<Message> messages = new ArrayList<Message>();
     private int port;
+
     public Server(int port) {
         this.port = port;
     }
 
-    public void serve() throws Exception {
-        ServerSocket serverSocket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
-        while (true) {
-            log.info("on select...");
-            final Socket socket = serverSocket.accept();
-            InputStream inputStream = socket.getInputStream();
 
-            byte[] buf = new byte[1024];
-            int nRead = inputStream.read(buf);
-            System.out.println(new String(buf, 0, nRead));
+
+    public void serve() {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        try {
+            serverSocket = new ServerSocket(port, 10, InetAddress.getByName("localhost"));
+            log.info("on select...");
+            while (true) {
+                socket = serverSocket.accept();
+                Potok potok = new Potok(socket, messages, list);
+                potok.setName("Client" + "[" + potok.getOrder() + "]@" + socket.getInetAddress() + ":" + socket.getPort());
+                System.out.println(potok.getName());
+                list.add(potok);
+                potok.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            IOUtils.closeQuietly(socket);
         }
     }
 
